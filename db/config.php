e2d4f7c1d82d5dab740b87b5d2190b68ad50fc01
poonamkhatri <59562098+poonamkhatri@users.noreply.github.com>
2021-11-18 21:35:51 +0500

    Add files via upload

--- db/config.php
+++ db/config.php
@@ -1,1 +1,11 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db = "crud";
 
+$conn = mysqli_connect($servername, $username, $password, $db);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
